fd3a670474e9a8603c6f5e088c68ba8badc9e19d
Katia de Paiva Lopes <katiaplopes@gmail.com>
2023-08-11 17:42:09 +0000

    WGCNA for monocytes

---
+++ 1_download_data.R
@@ -0,0 +1,15 @@
+# Katia Lopes
+# Aug 3, 2023
+# Download expression data 
+
+# https://www.bioconductor.org/packages/release/bioc/vignettes/ExpressionAtlas/inst/doc/ExpressionAtlas.html 
+# http://www.bioconductor.org/packages/release/bioc/vignettes/ArrayExpress/inst/doc/ArrayExpress.R
+
+# if (!require("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# 
+# BiocManager::install("ArrayExpress")
+
+library("ArrayExpress")
+
+
